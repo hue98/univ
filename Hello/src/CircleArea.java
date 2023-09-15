2a34cbbd9de4d6f0c6f7508c921861a4e69701c1
boou <boou@localhost>
2023-09-15 10:10:55 +0900

    CircleArea

---
+++ Hello/src/CircleArea.java
@@ -0,0 +1,16 @@
+
+public class CircleArea {
+
+	public static void main(String[] args) {
+		final double PI = 3.14; // 원주율을 상수로 선언
+		
+		double radius = 10.0; // 원의 반지
+		double circleArea = radius*radius*PI; // 원의 면적 계
+		
+		// 원의면적을 화면에 출력한다.
+		System.out.println("원의 면적 = " + circleArea);
+		// TODO Auto-generated method stub
+
+	}
+
+}
